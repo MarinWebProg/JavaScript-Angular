8a695c99c9ae25bcb352a0dbe06d7cade8157582
Sergio Alejandro Marin Sarmiento <sergio.marin@durango.gob.mx>
2022-11-07 16:53:33 -0600

    Seccion 26 - Apartado 94 en pause

---
+++ src/aprendiendo-jquery/js/01-selectores.js
@@ -0,0 +1,37 @@
+// $(document).ready(function(){
+//  console.log("Esta cargado")
+// }); //Esta funge como evento y tambien puede hacer callback
+
+$(document).ready(function(){
+    //Selector de ID
+    $("#rojo").css("background","red")
+    .css("color", "white");
+    
+    $("#amarillo").css("background","yellow")
+    .css("color", "green");
+   
+    $("#verde").css("background","green")
+    .css("color", "white");
+
+    //Selector de clases
+    var mi_clase = $('.zebra').css("padding","5px");
+   
+
+    $('.sin_borde').click(function(){
+        console.log("Click dado")
+        $(this).addClass('zebra')
+    })
+
+    //Selecector de etiqueta
+    var parrafos = $('p').css("cursor","pointer");
+
+    parrafos.click(function(){
+        var that = $(this);
+
+        if(!that.hasClass('zebra')){
+            that.addClass('grande');
+        }else{
+            that.removeClass('grande');
+        }
+    });
+})
