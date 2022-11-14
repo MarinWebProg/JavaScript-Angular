af848764443a0d5144b2f78816f39c16e08e5ef4
Sergio Alejandro Marin Sarmiento <sergio.marin@durango.gob.mx>
2022-11-14 16:52:18 -0600

    Avance de la parte 108 AJAX

--- src/aprendiendo-jquery/js/05-ajax.js
+++ src/aprendiendo-jquery/js/05-ajax.js
@@ -13,5 +13,49 @@ $(document).ready(function(){
         $("#datos").append("<p>"+element.first_name+ " "+element.last_name+"</p>")
     });
 
+    
+    $("#formulario").submit(function(e){
+        e.preventDefault();
+        var usuario = {
+            // name: "pepe",
+            // web:"unaweb.com"
+            name: $('input[name="name"]').val(),
+            web:$('input[web="web"]').val()
+        };
+        console.log(usuario);
+
+        $.post($(this).attr("action"), usuario, function(response){
+            console.log(response);
+        }).done(function(){
+            alert("Usuario Registrado");
+        });
+
+        // $.ajax({
+        //     type:'POST',
+        //     dataType:'json',
+        //     contentType:'application/json'
+        // })
+
+        $.ajax({
+            type:'POST',
+            URL: $(this).attr("action"),
+            data: usuario,
+            beforeSend: function(){
+                console.log("Enviando usuario...");
+            },
+            success: function(response){
+                console.log(response);
+            },
+            error:function(){
+                console.log("A ocurrido un erro")
+            }
+
+
+        });
+
+        return false;
+
+    });
+
    });
 });
