5fcf7a4925585b99d80374e72f79f4609ea243c3
sergio.marin <sergio.marin@durango.gob.mx>
2022-12-15 17:00:23 -0600

    Avance

--- src/angular/primerpaso/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ src/angular/primerpaso/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -1,10 +1,24 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Zapatilla } from '../models/zapatilla';
 
 @Component({
   selector: 'zapatillas',
   templateUrl: './zapatillas.component.html',
   styleUrls: ['./zapatillas.component.css']
 })
-export class ZapatillasComponent {
+export class ZapatillasComponent
+implements OnInit{
   public titulo: string = 'Componente de zapatillas/tennis'
+  public zapatillas: Array<Zapatilla>;
+
+  constructor(){
+    this.zapatillas=[
+      new Zapatilla('Reebox clasicones', 'Reebox', 'Blanco', 80, true)
+    ];
+  }
+
+  ngOnInit(){
+    console.log('Escribe lo que ocupes');
+  }
+
 }
